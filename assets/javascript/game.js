cc346e874c56f317f8f0ac2992c1e66dbd115190
Carlos Castrillon <carloscastrillon1991@gmail.com>
2019-10-25 00:41:17 -0400

    Version with the new functions remaining and winner

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,27 +21,27 @@ function GameStart(){
     }
 
     if(randomWordL === "cristianoronaldo"){
-        document.getElementById("hint").innerHTML = "He used to played for Real Madrid";
+        document.getElementById("hint").innerHTML = "He plays for Juventus and won five Ballon d’Or awards (player of the year).";
     }
 
     if(randomWordL === "leonelmessi"){
-        document.getElementById("hint").innerHTML = "He used to played for Barcelona";
+        document.getElementById("hint").innerHTML = "He plays for Barcelona and widely recognized as the best player in the world and rated by some commentators, coaches and players as the greatest footballer of all time.";
     }
 
     if(randomWordL === "jamesrodriguez"){
-        document.getElementById("hint").innerHTML = "He used to played for Bayer Munich";
+        document.getElementById("hint").innerHTML = "Colombian Soccer player that plays for Real Madrid";
     }
 
     if(randomWordL === "griezman"){
-        document.getElementById("hint").innerHTML = "He used to played for Atletico de Madrid";
+        document.getElementById("hint").innerHTML = "He plays for Barcelona and Recently won the World Cup in 2018";
     }
 
     if(randomWordL === "neymar"){
-        document.getElementById("hint").innerHTML = "He used to played for PSG";
+        document.getElementById("hint").innerHTML = "He plays for PSG and the most expensive purchase from the team";
     }
 
     if(randomWordL === "mbappe"){
-        document.getElementById("hint").innerHTML = "He used to played for Monaco";
+        document.getElementById("hint").innerHTML = "He Plays for PSG and He is the second teenager in history to score in a World Cup final.";
     }
 
 
@@ -93,11 +93,13 @@ function checkLetters(guesserPress){
 }
 
 function reset() {
-    guessesRem= 9;
+    guessesRem= 10;
     wrongGuess = [];
     blanksAndCorrect = [];
     document.getElementById("img").src = "./assets/images/guess.jpg";
     document.getElementById("reset").style.display = "none"; 
+    document.getElementById("remain").innerHTML = guessesRem;
+    document.getElementById("showgu").innerHTML = "  " + wrongGuess.join("  ");
     GameStart();
 }
 
@@ -105,6 +107,9 @@ function winner(){
    var  correctW = blanksAndCorrect.join("");
     console.log(correctW);
     if(randomWordL===correctW){
+        
+        if(correctW === "cristianoronaldo"){
+
         document.getElementById("img").src = "./assets/images/cristiano_ronaldo.jpg";
 
         document.getElementById("display").innerHTML = "CRISTIANO RONALDO";
@@ -119,6 +124,95 @@ function winner(){
     }
 
 
+    if(correctW === "leonelmessi"){
+
+        document.getElementById("img").src = "./assets/images/messi.jpg";
+
+        document.getElementById("display").innerHTML = "LEONEL MESSI";
+
+        document.getElementById("reset").style.display = "inline-block"; 
+
+        wins++;
+
+        document.getElementById("win").innerHTML = wins;
+
+        console.log("entro");
+    }
+
+    if(correctW === "griezman"){
+
+        document.getElementById("img").src = "./assets/images/griezman.jpg";
+
+        document.getElementById("display").innerHTML = "ANTOINE GRIEZMAN";
+
+        document.getElementById("reset").style.display = "inline-block"; 
+    
+        wins++;
+
+        document.getElementById("win").innerHTML = wins;
+
+        console.log("entro");
+    }
+
+    if(correctW === "jamesrodriguez"){
+
+        document.getElementById("img").src = "./assets/images/james.jpg";
+
+        document.getElementById("display").innerHTML = "JAMES RODRIGUEZ";
+
+        document.getElementById("reset").style.display = "inline-block"; 
+    
+        wins++;
+
+        document.getElementById("win").innerHTML = wins;
+
+        console.log("entro");
+    }
+
+    if(correctW === "neymar"){
+
+        document.getElementById("img").src = "./assets/images/neymar.jpg";
+
+        document.getElementById("display").innerHTML = "NEYMAR JR";
+
+        document.getElementById("reset").style.display = "inline-block"; 
+    
+        wins++;
+
+        document.getElementById("win").innerHTML = wins;
+
+        console.log("entro");
+    }
+
+    if(correctW === "mbappe"){
+
+        document.getElementById("img").src = "./assets/images/mbappe.jpg";
+
+        document.getElementById("display").innerHTML = "KYLLIAN MBAPPE";
+
+        document.getElementById("reset").style.display = "inline-block"; 
+    
+        wins++;
+
+        document.getElementById("win").innerHTML = wins;
+
+        console.log("entro");
+    }
+
+
+
+
+    }
+}
+
+
+
+function remaining(){
+
+    if(guessesRem===0){
+        document.getElementById("display").innerHTML = "TRY AGAIN";
+        reset();
+    }
 }
 
 
@@ -130,6 +224,6 @@ document.onkeyup = function(event) {
     var guesse = String.fromCharCode(event.keyCode).toLowerCase();
    checkLetters(guesse);
     winner();
-
+    remaining();
 
   };
